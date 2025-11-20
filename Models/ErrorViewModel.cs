f00d9f471b33f89a981e671b0460bb46636e6799
LabuschagneZain <165674325+LabuschagneZain@users.noreply.github.com>
2025-11-20 13:47:42 +0200

    SQL Update
    Updated project to SQL SMSS (MS SQL)

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ST10442835_PROG6212_CMCS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
